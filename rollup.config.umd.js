435fcba4cedb260ff195a2aedf4b9c6ebd277525
Trevor Buckner <calculuschild@gmail.com>
2026-01-28 19:45:39 -0500

    Update jest and eslint configs

--- rollup.config.umd.js
+++ rollup.config.umd.js
@@ -1,17 +1,17 @@
 export default [
-  {
-    input: 'src/index.js',
-    output: {
-      name: 'justified-paragraphs',
-      file: 'lib/index.umd.js',
-      format: 'umd'
-    }
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      file: 'lib/index.cjs',
-      format: 'cjs'
-    }
-  }
+	{
+		input  : 'src/index.js',
+		output : {
+			name   : 'marked-variables',
+			file   : 'lib/index.umd.js',
+			format : 'umd'
+		}
+	},
+	{
+		input  : 'src/index.js',
+		output : {
+			file   : 'lib/index.cjs',
+			format : 'cjs'
+		}
+	}
 ];
